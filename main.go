a60f6cd2e327ea10ac8942614ec106c406545713
Howard Mao <zhehao.mao@gmail.com>
2014-04-06 08:47:14 -0700

    add a -noconsole option

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ var rootdir = flag.String("rootdir", "./",
 			  "which directory to serve files from")
 var address = flag.String("address", ":8080",
 			  "address to bind server to")
+var noconsole = flag.Bool("noconsole", false,
+			"if present, don't take user input")
 
 const CommandBufferSize = 100
 var commandchan = make(chan string, CommandBufferSize)
@@ -58,7 +60,9 @@ func handleDebug(writer http.ResponseWriter, request *http.Request) {
 			return
 		}
 		fmt.Println(string(body))
-		fmt.Print("> ")
+		if !*noconsole {
+			fmt.Print("> ")
+		}
 		writer.Write([]byte("success"))
 	} else {
 		writer.WriteHeader(405)
@@ -75,7 +79,9 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	go handleInput(reader)
+	if !*noconsole {
+		go handleInput(reader)
+	}
 
 	fmt.Printf("serving %s from %s\n", *rootdir, *address)
 	err := http.ListenAndServe(*address, nil)
